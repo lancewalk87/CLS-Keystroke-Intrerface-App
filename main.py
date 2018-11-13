eab233ebb5fd24c16424bf2bf4339ad1f56c771e
Lance Walker <walkerlance65@gmail.com>
2018-11-13 07:46:26 -0700

    v1.0.2

--- main.py
+++ main.py
@@ -6,25 +6,19 @@
 * Copyright © 2018 CodeLife Productions. All rights reserved.
 '''
 
+# frameworks
 import kivy
-
 kivy.require('1.9.0')
-
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.button import Label
 
-
-# Inherit Kivy's App class which represents the window
-# for our widgets
-# HelloKivy inherits all the fields and methods
-# from Kivy
-class HelloKivy(App):
-
-    # This returns the content we want in the window
+# Class: Main
+class Main(App):
+    global Window
     def build(self):
-        # Return a label widget with Hello Kivy
-        return Label(text="Hello Kivy")
-
-
-helloKivy = HelloKivy()
-helloKivy.run()
+        self.title = "Keystroke Interface"
+        Window.clearcolor = (1, 1, 1, 0)
+        Window.size = (1024, 768)
+        return Label(text="Keystroke Interface")
+Main().run()
